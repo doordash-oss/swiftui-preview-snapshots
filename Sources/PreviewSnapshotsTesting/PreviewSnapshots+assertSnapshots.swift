a7660a989ada110f81f0f2d9d0581b1cc975a361
John Flanagan <john.flanagan@doordash.com>
2023-08-16 11:59:47 -0500

    Fix doc comment

--- Sources/PreviewSnapshotsTesting/PreviewSnapshots+assertSnapshots.swift
+++ Sources/PreviewSnapshotsTesting/PreviewSnapshots+assertSnapshots.swift
@@ -271,7 +271,7 @@ extension PreviewSnapshots {
 // MARK: Configuration name helper
 
 private extension PreviewSnapshots.Configuration {
-    /// Construct a snapshot name based on the configuration name and an optional suffix.
+    /// Construct a snapshot name based on the configuration name and an optional prefix.
     func snapshotName(prefix: String?) -> String {
         guard let prefix else { return name }
         return "\(prefix)-\(name)"
